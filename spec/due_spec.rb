9ff8eb108ebd9977fca71de8a10c1bd94c8e6500
lauretteroy <laurieroy.dev@gmail.com>
2021-10-18 12:38:19 -0700

    Review hooks and let block

--- spec/due_spec.rb
+++ spec/due_spec.rb
@@ -1,20 +1,46 @@
 require 'due/due'
 RSpec.describe 'a due summary' do
-	it 'has minutes' do
-		due_summary = Due::Due.new('2021-10-11 12:00:00')
-		due_summary.now_date_time = Time.parse('2020-10-12 12:00:00')
+	let(:due_summary) {Due::Due.new('2021-10-11 12:00:00')}
 
-		due_summary.calc_difference
+	# before(:example, do_setup: true) do
+	# 	puts 'before...'
+	# 	@due_summary = Due::Due.new('2021-10-11 12:00:00')
+	# end
 
-		expect(due_summary.calc_mins_till).to(eq(524160))
+	# after(:example, do_setup: true) do
+	# 	@due_summary = nil
+	# end
+
+	# around(:example, do_setup: true) do |ex|
+	# 	@due_summary = Due::Due.new('2021-10-11 12:00:00')
+	# 	ex.run
+	# 	@due_summary = nil
+	# end
+
+	context 'when working out the difference', do_setup: true do
+		it 'has minutes' do
+			
+			due_summary.now_date_time = Time.parse('2020-10-12 12:00:00')
+
+			due_summary.calc_difference
+
+			expect(due_summary.calc_mins_till).to(eq(524_160))
+		end
+
+		it 'has hours' do
+			due_summary.now_date_time = Time.parse('2020-10-12 12:00:00')
+
+			due_summary.calc_difference
+
+			expect(due_summary.calc_hours_till).to(eq(8_736))
+		end
 	end
 
-	it 'has hours' do
-		due_summary = Due::Due.new('2021-10-11 12:00:00')
-		due_summary.now_date_time = Time.parse('2020-10-12 12:00:00')
-
-		due_summary.calc_difference
-
-		expect(due_summary.calc_hours_till).to(eq(8736))
+	context 'when given hardcoded values' do
+		it 'has teh same minutes' do
+			expect(1).to eq(1)
+		end
 	end
+
+
 end
